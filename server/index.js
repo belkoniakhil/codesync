58f07ae082b5482af98bd0ffb4d73e04a079547b
Akhil Kumar Belkoni <belkoniakhil9@gmail.com>
2026-06-17 02:05:34 +0530

    Fix production Socket.IO CORS

--- server/index.js
+++ server/index.js
@@ -20,10 +20,10 @@ Attach Socket.IO to HTTP Server
 */
 const io = new Server(server, {
   cors: {
-  origin: [
-  "http://localhost:5173",
- "https://codesync-xyz.vercel.app",
-],
+    origin: [
+      "http://localhost:5173",
+      "https://codesync-dtvrelxg7-akhilbelkoni-s-projects.vercel.app",
+    ],
     methods: ["GET", "POST"],
   },
 });
